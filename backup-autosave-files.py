6eb45cc04e3848edeeaa756ff594263768f50880
Andrew Wilson <andrew.wilson@diamond.ac.uk>
2016-03-10 16:19:11 +0000

    Further refactoring. Testing with local files.

--- backup-autosave-files.py
+++ backup-autosave-files.py
@@ -5,9 +5,16 @@ import os, shutil, time, re, sys
 
 class AutosaveBackup:
     
+    def isAnIoc(self, inputStr):
+        iocDirectoryPattern = r"^BL[0-9]{2}[IJ]-[A-Z]{2}-IOC-[0-9]{2}$"
+        matches = re.match(iocDirectoryPattern, inputStr)
+        if matches is not None:
+            return True
+        else:
+            return False
+
     def scanSourceDirs(self):
         lastDirectoryPattern = r"\S+/(\S+)$"
-        iocDirectoryPattern = r"BL[0-9]{2}[IJ]-[A-Z]{2}-IOC-[0-9]{2}"
         
         directories = os.listdir(self.autosaveTop)
         
@@ -17,10 +24,9 @@ class AutosaveBackup:
         
         for onedir in directories:
             print onedir,
-            dirmatches = re.match(iocDirectoryPattern, onedir)
-            if dirmatches is not None:
-                currentdir = dirmatches.group(0)
-                print "looks like dir for", currentdir
+            if self.isAnIoc(onedir):
+                currentdir = onedir
+                print "looks like an IOC dir"
                 dirsToSearch.append(currentdir)
             else:
                 print "doesn't look like an IOC dir."
@@ -49,16 +55,37 @@ class AutosaveBackup:
                 
         return latestFiles
         
-    def __init__(self):
-        self.bl = "i13"
+    def createBackupSubdir(self, subdir):
+        # cd to backup directory
+        os.chdir(self.backupTarget)
+        
+        # Create a directory with date and time
+        print "Create subdirectory {0} in {1}".format(subdir, self.backupTarget)
+        
+        os.mkdir(subdir)
+        
+    def createBackupDir(self):
+        # cd to backup top level
+        os.chdir(self.backupTop)
+        
+        # Check new directory doesn't already exist
+        if os.path.exists(self.newDirectory):
+            print "New directory {0} in {1} already exists. Exiting.".format(self.newDirectory, self.backupTop)
+            sys.exit()
+        else:
+            print "New directory {0} in {1} doesn't already exist: OK to create it".format(self.newDirectory, self.backupTop)
+            os.mkdir(self.newDirectory)
+        
+    def __init__(self, bl, whichIoc):
+        self.bl = bl
+        self.whichIoc = whichIoc
 
         self.autosaveTop = "/home/tdq39642/tmp/autosave-for-testing/"
         self.backupTop = "/home/tdq39642/tmp/autosave_backup/{0}".format(self.bl)
         
         # Name for new backup subdirectory
-        self.newDirecoty = "autosave_backup_{0}".format(time.strftime("%Y-%m-%d_%H%M%S"))
-
-        print "Top level autosave dir is "+self.autosaveTop+": what's inside?\n"
+        self.newDirectory = "autosave_backup_{0}".format(time.strftime("%Y-%m-%d_%H%M%S"))
+        self.backupTarget = os.path.join(self.backupTop, self.newDirectory)
         
         # Check source and destination paths are OK
         if not os.path.exists(self.autosaveTop):
@@ -67,6 +94,26 @@ class AutosaveBackup:
         if not os.path.exists(self.backupTop):
             print "Target top level directory for backing up files does not seem to exist: {0}".format(self.backupTop)
             sys.exit()
+            
+        # Check which IOC is valid
+        if not self.isAnIoc(self.whichIoc):
+            if not (self.whichIoc == "all"):
+                self.printUsage()
+                sys.exit()
+            
+        print "We will back up the latest autosave files for {0}, ioc {1}".format(self.bl, self.whichIoc)
+        print " from {0}".format(self.autosaveTop)
+        print " to {0}".format(self.backupTarget)
+        user = raw_input( "Is this right? (y/n) " )
+        
+        matches = re.match(r"^y", user)
+        
+        if (matches is None):
+            print "Exiting."
+            sys.exit()
+        
+        
+        self.createBackupDir()
         
         # Initialise the list of files we will want to back up
         self.backupFileList = dict()
@@ -81,44 +128,36 @@ class AutosaveBackup:
         print "\nLooking for files..."
         for onedir in dirsToSearch:
             self.backupFileList[onedir] = self.findLatestFilesIn(onedir)
-            
-            
-        
-    #    for root, dirs, files in os.walk(self.autosaveTop):
-    #        print root, "has", len(files), ("file" if len(files) == 1 else "files"),
-    #        print "and", len(dirs), ("directory" if len(dirs) == 1 else "directories")
-    #        
-    #        if (len(files) > 0):
-    #            
-    #            # Get last directory component
-    #            dirmatches = re.match(lastDirectoryPattern, root)
-    #            
-    #            currentdir = dirmatches.group(1)
-    #            
-    #            for thisFile in files:
-    #                # The three most recent files are those that end in .sav without a suffix.
-    #                matches = re.match(latestFilePattern, thisFile)
-    #                
-    #                if (matches is not None):
-    #
-    #                    fileToBackUp = os.path.join(currentdir, thisFile)
-    #                    print thisFile
-    #                    self.backupFileList.append(fileToBackUp)
                 
         print "\nList of files we will back up:"
         for subdir in self.backupFileList:
-            print subdir
+            
+            self.createBackupSubdir(subdir)
+            
+            
             for fileToBackUp in self.backupFileList[subdir]:
-                print " "+fileToBackUp
-            
-        # cd to backup directory
-        #os.chdir(self.backupTop)
+                
+                sourcepath = os.path.join(self.autosaveTop,subdir,fileToBackUp)
+                destpath = os.path.join(self.backupTarget,subdir)
+                print "Copy {0} to {1} .".format(sourcepath, destpath)
+                shutil.copy2(sourcepath, destpath)
+    
+    @staticmethod       
+    def printUsage():
+        print "Usage:"
+        print "backup-autosave-files.py <bl> <ioc> [<more iocs>]"
+        print "backup-autosave-files.py <bl> all"
         
-        # Create a directory with date and time
-        #os.mkdir()
-            
-            
-    #    print "Backup dir will be:", self.backupTop
+    
+
     
 if __name__ == "__main__":
-    a = AutosaveBackup()
+    if (len(sys.argv) < 3):
+        AutosaveBackup.printUsage()
+        sys.exit()
+        
+    bl = sys.argv[1]
+    
+    whichIoc = sys.argv[2]
+    
+    a = AutosaveBackup(bl, whichIoc)
