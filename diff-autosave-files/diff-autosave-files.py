14ad5de39e5dcf3215ada5f564e8193781451caf
Andrew Wilson <andrew.wilson@diamond.ac.uk>
2016-03-15 16:20:49 +0000

    Proof of concept to diff two autosave files and make an html output

--- diff-autosave-files/diff-autosave-files.py
+++ diff-autosave-files/diff-autosave-files.py
@@ -7,7 +7,40 @@ import sys, difflib
 class AutosaveDiffer():
     
     def __init__(self):
-        print "Hello world"
+        backupFilename = "bk_0.sav"
+        liveFilename = "live_0.sav"
+        headFilename = "head.html"
+        footFilename = "foot.html"
+        outFilename = "out.html"
+        
+        backupLines = ['']
+        liveLines = ['']
+        headLines = ['']
+        footLines = ['']
+        
+        # Open input files
+        with open(backupFilename) as backupFile:
+            backupLines = backupFile.readlines()
+        with open(liveFilename, 'r') as liveFile:
+            liveLines = liveFile.readlines()
+        with open(headFilename, 'r') as headFile:
+            headLines = headFile.readlines()
+        with open(footFilename, 'r') as footFile:
+            footLines = footFile.readlines()
+        
+        # Open output file
+        with open(outFilename, 'w') as outFile:
+
+            # Generate the diff
+            d = difflib.HtmlDiff()
+            diffTable = d.make_table(backupLines, liveLines, fromdesc='Backup', todesc='Live file', context=True, numlines=0)
+            
+            finalLines = headLines + [diffTable] + footLines
+            
+            for line in finalLines:
+                outFile.write(line)
+        
+        
         
 if __name__ == "__main__":
     
