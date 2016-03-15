51db62e6d48455f29e9a4a6e84930dcf35f25faf
Andrew Wilson <andrew.wilson@diamond.ac.uk>
2016-03-15 16:38:37 +0000

    Refactoring

--- diff-autosave-files/diff-autosave-files.py
+++ diff-autosave-files/diff-autosave-files.py
@@ -6,39 +6,56 @@ import sys, difflib
 
 class AutosaveDiffer():
     
-    def __init__(self):
-        backupFilename = "bk_0.sav"
-        liveFilename = "live_0.sav"
-        headFilename = "head.html"
-        footFilename = "foot.html"
-        outFilename = "out.html"
-        
+    def makeDiff(self, backupFilename, liveFilename):
+        # Lists to hold lines read from files
         backupLines = ['']
         liveLines = ['']
-        headLines = ['']
-        footLines = ['']
         
         # Open input files
-        with open(backupFilename) as backupFile:
+        with open(backupFilename, 'r') as backupFile:
             backupLines = backupFile.readlines()
         with open(liveFilename, 'r') as liveFile:
             liveLines = liveFile.readlines()
-        with open(headFilename, 'r') as headFile:
-            headLines = headFile.readlines()
-        with open(footFilename, 'r') as footFile:
-            footLines = footFile.readlines()
+            
+        # Generate the diff
+        d = difflib.HtmlDiff()
+        diffTable = d.make_table(backupLines, liveLines, fromdesc='Backup', todesc='Live file', context=True, numlines=0)
+        return diffTable
         
+    def writeFile(self, outFilename, finalLines):
         # Open output file
         with open(outFilename, 'w') as outFile:
-
-            # Generate the diff
-            d = difflib.HtmlDiff()
-            diffTable = d.make_table(backupLines, liveLines, fromdesc='Backup', todesc='Live file', context=True, numlines=0)
-            
-            finalLines = headLines + [diffTable] + footLines
-            
+            # Write output line by line
             for line in finalLines:
                 outFile.write(line)
+                
+    def readHtmlTemplate(self):
+        # Read header and footer lines
+        with open(self.headFilename, 'r') as headFile:
+            self.headLines = headFile.readlines()
+        with open(self.footFilename, 'r') as footFile:
+            self.footLines = footFile.readlines()
+    
+    def __init__(self):
+        backupFilename = "bk_0.sav"
+        liveFilename = "live_0.sav"
+        self.headFilename = "head.html"
+        self.footFilename = "foot.html"
+        outFilename = "out.html"
+        
+        self.headLines = ['']
+        self.footLines = ['']
+        
+        # Get the lines from the HTML header and footer
+        self.readHtmlTemplate()
+        
+        # Generate the diff
+        diffTable = self.makeDiff(backupFilename, liveFilename)
+        
+        # Build list of lines to write
+        finalLines = self.headLines + [diffTable] + self.footLines
+        self.writeFile(outFilename, finalLines)
+
         
         
         
