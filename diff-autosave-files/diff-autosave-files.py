24f1147f5da5c46f09df81462c2911b8924257d1
Andrew Wilson <andrew.wilson@diamond.ac.uk>
2016-03-15 15:44:43 +0000

    Commit first version of script to diff backup and live autosave files

---
+++ diff-autosave-files/diff-autosave-files.py
@@ -0,0 +1,14 @@
+#!/dls_sw/prod/tools/RHEL6-x86_64/defaults/bin/dls-python
+# Script to generate a diff between backed up and live autosave files for a beamline
+# producing an HTML output file
+
+import sys, difflib
+
+class AutosaveDiffer():
+    
+    def __init__(self):
+        print "Hello world"
+        
+if __name__ == "__main__":
+    
+    d = AutosaveDiffer()
