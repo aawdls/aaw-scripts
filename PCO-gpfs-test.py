793d369e338c82b619c12e8a6198206c7a307a0a
Andrew Wilson <andrew.wilson@diamond.ac.uk>
2016-08-09 17:24:39 +0100

    Wireframing tests

--- PCO-gpfs-test.py
+++ PCO-gpfs-test.py
@@ -7,25 +7,20 @@ import time
 
 
 
-class pcoHdfTest():
-    
-    # Test paramters
-    numImages       = 1000
-    numAcquisitions = 1000
-    exposureTime    = 0.005
-    acquirePeriod   = 0.006
-    visit           = "cm14467-3"
-    filePath        = "G:/i13/data/2016/{0}/tmp".format(visit)
-    fileName        = "filetest"
-    pvPrefix        = "BL13I-EA-DET-02"
-    camPrefix       = ":CAM"
-    hdfPrefix       = ":HDF5"
+class pcoHdfTest()
+"""Sets up and runs a repeatable test of writing files from a PCO camera IOC.
+"""
     
     def setupIoc(self):
         """
         Set up the IOC with parameters for the test
         """
-
+        
+        # Handy local variables
+        pvPrefix = self.parameters["pvPrefix"]
+        camPrefix = self.parameters["camPrefix"]
+        hdfPrefix = self.parameters["hdfPrefix"]
+        
         # Set up the IOC
         # Configure CAM plugin
         caput(pvPrefix + camPrefix + ":AcquireTime",        exposureTime,      wait=True, timeout=5)
@@ -44,15 +39,71 @@ class pcoHdfTest():
         caput(pvPrefix + hdfPrefix + ":AutoIncrement",     1,           wait=True, timeout=5)
         caput(pvPrefix + hdfPrefix + ":NumCapture",        numImages,   wait=True, timeout=5)
 
-    def __init__(self):
+    def startOneAcquisition(self):
+        """
+        Start a single file acquisition
+        """
+        caput(pvPrefix + camPrefix + ":Acquire",        1,      wait=True, timeout=5)
+        
+    def inProgress(self):
+        """
+        Returns true if acquisition is in progress or file is open
+        """
+        
+    def getStats(self):
+        """
+        Grab the diagnostics we need 
+        """
+        # Write time, write speed, dropped frames, successful frames, total faults, write errors
+        
+    def runTests(self):
+        """
+        Run the tests in a loop
+        """
+        for (testIndex in range(0, parameters["numFiles"])):
+            
+            # Start the acquisition
+            self.startOneAcquisition()
+            
+            # Wait for it to complete
+            while (self.inProgress()):
+                time.sleep(1)
+                # Drop out if we think it's stuck
+                if (self.timedOut()):
+                    break
+            
+            # Check for problems
+            if (self.problem()):
+                # Handle error
+                # If recoverable, recover, and continue with the next acquisition
+                break
+                
+            # Get diagnostics for this acquisition
+            self.getStats()
+
+    def __init__(self, testParams):
         """
         Set up a test; wait for user input to run
         """
-        setupIoc()
+        self.parameters = testParams
+        self.setupIoc()
         
 #get the value (loop until the counter is 'done')
 #count = caget("BL12I-EA-DET-01:SCALER.S18", datatype=DBR_LONG )
 
 if __name__ == "__main__":
     
-    t = pcoHdfTest()
+    # Define test parameters
+    testParams = {
+        numImagesPerFile:   1000,
+        numFiles:           1000,
+        exposureTime:       0.005,
+        acquirePeriod:      0.006,
+        filePath:           "G:/i13/data/2016/cm14467-3/tmp",
+        fileName:           "filetest",
+        pvPrefix:           "BL13I-EA-DET-02",
+        camPrefix:          ":CAM",
+        hdfPrefix:          ":HDF5" }
+    
+    # Begin the test
+    t = pcoHdfTest(testParams)
