e32d6b70e8594d70d302ab7b6802d5dfc1cfc78c
Andrew Wilson <andrew.wilson@diamond.ac.uk>
2017-04-13 16:00:58 +0100

    Now works with an argument

--- get-ioc-path/get-ioc-path.py
+++ get-ioc-path/get-ioc-path.py
@@ -1,87 +1,108 @@
 import subprocess
 import re
+import os.path, sys
 
-"""Generate a BL module configure/RELEASE file from the currently configured versions of a list of IOCs"""
+"""Parse a BL module configure/RELEASE file and check the versions of IOCs against those in the redirector"""
 
-# List of macros to be substituted in paths
-macros = {"$(PROD_IOCS)": "/dls_sw/prod/R3.14.12.3/ioc",
-            "$(WORK_IOCS)": "/dls_sw/work/R3.14.12.3/ioc",
-            "$(BUILDER_IOCS)": "/dls_sw/work/R3.14.12.3/support/BL13I-BUILDER/iocs"}
+class ReleaseFileIocChecker:
+    
+    def __init__(self, filename):
+        # This will hold the list of macros we find in the RELEASE file
+        self.macros = {}
+        self.filename = filename
+        self.input_file = None
 
-# List of IOCs to check
-iocs = {
-        "MO2I": "BL13I-MO-IOC-02",
-        "MO3I": "BL13I-MO-IOC-03",
-        "MO4I": "BL13I-MO-IOC-04",
-        "MO5I": "BL13I-MO-IOC-05",
-        "MO24I": "BL13I-MO-IOC-24",
-        "DI1I": "BL13I-DI-IOC-01",
-        "DI10I": "BL13I-DI-IOC-10",
-        "DI16I": "BL13I-DI-IOC-16",
+
+    def configure_ioc_show(self, ioc_name):
+        """
+        Run "configure-ioc show <ioc_name>" and return the path with macros applied
+        """
         
-        }
+        try:
+            output = subprocess.check_output(["configure-ioc", "show", ioc_name]).split()
         
-def get_running_version(ioc_name):
-    
-    try:
-        output = subprocess.check_output(["configure-ioc", "show", ioc_name]).split()
-    
-    except:
-        return "NOT FOUND"
-    
-    path = output[1]
-    
-    for macro, expansion in macros.iteritems():
-        path = path.replace(expansion, macro)   
+        except:
+            return "NOT FOUND"
         
-    tokens = path.split("/bin")
-    
-    path = tokens[0]
-    
-    return path
-    
-input_file = open("RELEASE_TEST", "r")
-output_file = open("RELEASE_OUTPUT", "w")
-
-"""
-for macro, expansion in macros.iteritems():
-    lines.append("%s = %s" %(macro, expansion))
-"""
-
-
-for line in input_file:
-
-    if line.find("-IOC-") >= 0:
+        # Second word returned by configre-ioc is the path
+        path = output[1]
         
-        # Extract IOC name...
-        tokens = line.split("/")
-        for token in tokens:
-            if token.find("-IOC-") >= 0:
-                ioc_name = token
+        # Apply macros from configure-ioc
+        for macro, expansion in self.macros.iteritems():
+            macro_with_dollar = "$(%s)" % (macro)
+            path = path.replace(expansion, macro_with_dollar)   
+            
+        # Throw away the script name, only care about the directory
+        tokens = path.split("/bin")
+        path = tokens[0]
         
-        if ioc_name:
+        return path
+        
+    def check(self):
+    
+        # Open the RELEASE File
+        if os.path.exists(self.filename):
+            self.input_file = open(self.filename, "r")
+            print "Checking input file %s\n" % self.filename
+        else:
+            print "Cannot open input file %s" % self.filename
+            return
+            
 
+        # Parse the RELEASE file
+        for line in self.input_file:
+
+            # Naively look for macros
+            if line.find("/dls_sw/") >= 0:
+                # Assume this is a macro definition
                 tokens = line.split("=")
-                path_in_RELEASE = tokens[1].strip()
+                if len(tokens) == 2:
+                    key = tokens[0].strip()
+                    value = tokens[1].strip()
+                    self.macros[key] = value
+                    print "$(%s) = %s" %(key, value)
+                    
+            # Naively look for IOCs
+            elif line.find("-IOC-") >= 0:
                 
-                path_in_redirector = get_running_version(ioc_name)
-                if (path_in_RELEASE == path_in_redirector):
-                    print "%s matches redirector" % (ioc_name)
-                else:
-                    print ioc_name, "in RELEASE:", path_in_RELEASE, "in redirector:", path_in_redirector
-                    
-                throwAway = raw_input("Hit return to do next")
-                    
+                # Extract IOC name...
+                tokens = line.split("/")
+                for token in tokens:
+                    if token.find("-IOC-") >= 0:
+                        ioc_name = token.strip()
+                
+                # Got one!
+                if ioc_name:
 
-input_file.close()
-output_file.close()
+                        # So messy...
+                        tokens = line.split("=")
+                        path_in_RELEASE = tokens[1].strip()
+                        
+                        # Ask the redirector
+                        path_in_redirector = self.configure_ioc_show(ioc_name)
+                        
+                        # Are they the same?
+                        if (path_in_RELEASE == path_in_redirector):
+                            print "%s matches redirector" % (ioc_name)
+                        else:
+                            print ioc_name, "\tIn RELEASE:", path_in_RELEASE, "in redirector:", path_in_redirector
+                            
+                        # Debug breakpoint...
+                        #throwAway = raw_input("Hit return to do next")
 
-"""
+    def __enter__(self):
+        return self
+         
+    def __exit__(self, exc_type, exc_value, traceback):
+        # Close file
+        if self.input_file:
+            self.input_file.close()
 
-for label, ioc_name in iocs.iteritems():
-    path = get_running_version(label, ioc_name)
-    lines.append("%s = %s" %(label, path))
+if __name__ == "__main__":
     
-for line in lines:
-    print line
-"""
+    if (len(sys.argv) != 2):
+        print "Usage: get-ioc-path.py <Path to configure/RELEASE>"
+        exit()
+    
+    with ReleaseFileIocChecker(sys.argv[1]) as p:
+        p.check()
