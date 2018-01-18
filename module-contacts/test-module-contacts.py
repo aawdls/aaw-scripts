c07f52027d81f33ca5ea4d9d71c0d111f1f41811
Andrew Wilson <andrew.wilson@diamond.ac.uk>
2018-01-18 16:15:10 +0000

    Refactoring

--- module-contacts/test-module-contacts.py
+++ module-contacts/test-module-contacts.py
@@ -19,7 +19,7 @@ import dls_environment
 import dls_dependency_tree
 import argparse
 
-import list_template
+import module_contacts_html_template as html
 
 def get_git_module_list(git_server, area):
     """
@@ -111,18 +111,62 @@ class SupportModule():
         self.cc = cc_contact
         return contact, cc_contact
 
+def count_up_IOC_dependencies():
+    """Count occurences of support modules in dependency tress of IOCs in redirector
+
+    :return IOC_dependency(dict) key: module name, value: count of occurrences"""
+
+    startup_files = dict(
+        [line.split() for line in os.popen('configure-ioc list')])
+
+    IOC_dependency = {}
+
+    logging.debug("Counting up support modules depended on by IOCs")
+    # Get dependency tree for each IOC
+    for ioc in startup_files.keys():
+
+        # Some basic filters
+        if ioc.startswith("BL") \
+                and "-PS-" not in ioc and "BL-" not in ioc and "BL01B" not in ioc \
+                and "-SIM-" not in ioc and "sim" not in ioc:
+
+            # Get dependency tree for this IOC
+            logging.debug("ioc %s" % ioc)
+            path = "/".join(startup_files[ioc].split("/")[:-3])
+            tree = dls_dependency_tree.dependency_tree(None, module_path=path)
+            if tree.name and tree.name != "BLInterface" and tree.name != "ioc":
+                for leaf in tree.leaves:
+                    ss_ioc_name = tree.name
+                    ss_module = leaf.name
+                    ss_version = leaf.version
+
+                    # For each module, increment the counter
+                    if ss_module in IOC_dependency:
+                        IOC_dependency[ss_module] += 1
+                    else:
+                        IOC_dependency[ss_module] = 1
+
+    return IOC_dependency
+
+def parse_arguments():
+    """Parse command line arguments"""
+    help = """Write an HTML file with a list of support modules
+        in SVN and git, with their module contacts and the number
+        of IOCs in which they are direct dependencies."""
+    argument_parser = argparse.ArgumentParser(description=help)
+    argument_parser.add_argument("output_filename", type=str,
+                                 help="Path to output HTML file")
+    args = argument_parser.parse_args()
+    return args
+
 def main():
 
-    # Parse arguments
-    help="""Write an HTML file with a list of support modules
-    in SVN and git, with their module contacts and the number
-    of IOCs in which they are direct dependencies."""
-    argument_parser = argparse.ArgumentParser(description=help)
-    argument_parser.add_argument("-o","--output_filename", type=str, help="Path to output HTML file", nargs=1)
-    args = argument_parser.parse_args()
+    args = parse_arguments()
+
 
     logging.basicConfig(level=logging.DEBUG)
     area = "support"
+    logging.debug("Output HTML will be written to %s" % args.output_filename)
 
     # Git module list
     logging.debug("Getting list of Modules in %s area of git" % area)
@@ -135,7 +179,7 @@ def main():
     svn_server = svnClient()
     svn_module_list = get_svn_module_list(svn_server, area)
 
-    # We assume that if a module exists in both, it has been moved to git
+    # We assume that if a module exists in both SVN and git, it has been moved to git
     for module_name in svn_module_list:
         if module_name in git_module_list:
             svn_module_list.remove(module_name)
@@ -153,53 +197,20 @@ def main():
 
     # Look for occurrences in IOCs
     logging.debug("Getting list of IOCs in redirector")
-    startup_files = dict([line.split() for line in os.popen('configure-ioc list')])
-
-    IOC_dependency = {}
-
-    logging.debug("Counting up support modules depended on by IOCs")
-    # Get dependency tree for each IOC
-    for ioc in startup_files.keys():
-        # Some basic filters
-        if ioc.startswith("BL") \
-           and "-PS-" not in ioc and "BL-" not in ioc and "BL01B" not in ioc\
-           and "-SIM-" not in ioc and "sim" not in ioc:
-            # Get dependency tree for this IOC
-            logging.debug("ioc %s" % ioc)
-            path = "/".join(startup_files[ioc].split("/")[:-3])
-            tree = dls_dependency_tree.dependency_tree(None,module_path=path)
-            if tree.name and tree.name != "BLInterface" and tree.name != "ioc":
-                for leaf in tree.leaves:
-                    ss_ioc_name = tree.name
-                    ss_module = leaf.name
-                    ss_version = leaf.version
-
-                    # For each module, increment the counter
-                    if ss_module in IOC_dependency:
-                        IOC_dependency[ss_module] += 1
-                    else:
-                        IOC_dependency[ss_module] = 1
+    IOC_dependency = count_up_IOC_dependencies()
 
     # Simple display of results
     row_counter = 0
     with open(args.output_filename, "wb") as output_file:
 
-        a_row = """
-    <tr>
-        <td>%s</td>
-        <td>%s</td>
-        <td>%s</td>
-        <td>%s</td>
-        <td>%d</td>
-    </tr>
-"""
         logging.debug("Printing HTML header...")
-        output_file.write(list_template.page_head)
-        output_file.write(list_template.body_start)
-        output_file.write(list_template.table_head)
+        output_file.write(html.page_head)
+        output_file.write(html.body_start)
+        output_file.write(html.table_head)
 
-        logging.debug("Getting module contacts.")
+        logging.debug("Getting module contacts...")
         number_of_rows = len(module_list)
+
         for this_module in module_list:
 
             row_counter += 1
@@ -218,12 +229,12 @@ def main():
                 contact, cc = this_module.get_git_module_contact(git_server)
 
             # Write a row of output
-            output_file.write(a_row % (this_module.module_name, this_module.vcs, contact, cc, number_of_IOCs))
+            output_file.write(html.one_row % (this_module.module_name, this_module.vcs, contact, cc, number_of_IOCs))
             output_file.flush()
 
         logging.debug("Printing HTML footer")
-        output_file.write(list_template.table_bottom)
-        output_file.write(list_template.page_end)
+        output_file.write(html.table_bottom)
+        output_file.write(html.page_end)
 
         logging.debug("Script finished.")
 
